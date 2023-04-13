777e2b048b18d1b98947e902b521da3a9667cf30
kwonjh0825 <ghr1700@naver.com>
2023-04-13 15:56:31 +0900

    accounts app develop

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
-
 from django.urls import path
+from . import views
 
 app_name = "accounts"
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('update/', views.update, name='update'),
+    path('delete/', views.delete, name='delete'),
+    path('change_password/', views.delete, name='change_password'),
+    path('mypage/<int:accounts_pk>/', views.mypage, name='mypage'),
 ]
